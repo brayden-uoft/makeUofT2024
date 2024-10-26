60d4419f008fd732279333fe86095a928368fd26
jeffrey <arroyogreat@gmail.com>
2024-10-26 15:07:15 -0400

    implement homograph finding

--- src/homograph.js
+++ src/homograph.js
@@ -1,12 +1,38 @@
-fetch('JSONFilePath').then().then().catch();
+const fs = require('fs');
+const path = require('path');
 
+const libdir = path.resolve(__dirname);
+const hgdbFile = fs.readFileSync(path.join(libdir, 'homographs.json'), 'utf-8');
+const hgdb = JSON.parse(hgdbFile);
 
-function is_char_homoglyphic(char1, char2) {
-    if (char1 == char2) {
-        return true
+// Checks whether two individual characters are equivalent
+function isCharHomoglyphic(letter1, letter2) {
+    if (letter1 === letter2) {
+        return true;
     }
-    //function in python
-    //return letter2 in [entry['char'] for entry in hgdb[letter1]['similar_char']]
-    console.log(hgdb[letter1]['similar_char'].some(entry => entry['char'] == letter2));
-    return hgdb[letter1]['similar_char'].some(entry => entry['char'] == letter2);
+    return hgdb[letter1]['similar_char'].some(entry => entry['char'] === letter2);
 }
+
+function looksSimilar(domain1, domain2) {
+    /**
+     * Determine whether two domains are homographic (visually equivalent or nearly so)
+     */
+    domain1 = domain1.toLowerCase();
+    domain2 = domain2.toLowerCase();
+
+    if (domain1.length !== domain2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < domain1.length; i++) {
+        const letter1 = domain1[i];
+        const letter2 = domain2[i];
+
+        if (!isCharHomoglyphic(letter1, letter2)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
