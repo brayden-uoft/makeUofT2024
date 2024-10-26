8124d65973eed546fef9e3e8ca554579e1d9616e
brayden-hz <brayden@hepzibah.ai>
2024-10-26 13:55:06 -0400

    added url logger

---
+++ scripts/content.js
@@ -0,0 +1,7 @@
+console.log("ProjectT loaded");
+
+// Get the URL of the current site
+const currentURL = window.location.href;
+
+// Print the URL to the console
+console.log("Current URL:", currentURL);
