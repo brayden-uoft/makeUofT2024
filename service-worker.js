ff8776ae99ff87872e221e6f6c0cf1dff3a2face
Keaton <keaton.bailie@mail.utoronto.ca>
2024-10-26 14:00:14 -0400

    fixed panel

--- service-worker.js
+++ service-worker.js
@@ -8,18 +8,12 @@ chrome.sidePanel
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     if (!tab.url) return;
     const url = new URL(tab.url);
-    // Enables the side panel on google.com
-    if (url.origin === GOOGLE_ORIGIN) {
-        await chrome.sidePanel.setOptions({
-            tabId,
-            path: 'sidepanel.html',
-            enabled: true
-        });
-    } else {
-        // Disables the side panel on all other sites
-        await chrome.sidePanel.setOptions({
-            tabId,
-            enabled: false
-        });
-    }
+
+    // Enables the side panel
+    await chrome.sidePanel.setOptions({
+        tabId,
+        path: 'sidepanel.html',
+        enabled: true
+    });
+
 });
